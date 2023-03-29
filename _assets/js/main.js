e174a175d5a715258baf4a485a7c78de3df7ca23
CarloBattista <carlitobatti@gmail.com>
2023-03-29 13:47:23 +0200

    array lista spesa

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -4,3 +4,16 @@ Data una lista della spesa, stampare sulla pagina (anche brutalmente,
 basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 
 */
+
+let listaSpesa = [
+    "Pane",
+    "Patate",
+    "Olio",
+    "Pasta",
+    "Burro",
+    "Pomodori",
+    "Insalata",
+    "Zucchero",
+    "Riso",
+    "olive",
+];
