092344874a44e72ca14eb21180894bf110ec37dd
CarloBattista <carlitobatti@gmail.com>
2023-03-29 16:42:22 +0200

    input funzionante

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -6,12 +6,7 @@ basta che si vedano) gli elementi della lista individualmente con un ciclo while
 */
 
 // Lista della spesa
-let listaSpesa = [
-    "Pane",
-    "Patate",
-    "Olio",
-    "Pasta",
-];
+let listaSpesa = [];
 
 let spesa = 0;
 let shoppingList = document.querySelector(".shoppingList");
@@ -30,6 +25,41 @@ while (spesa < listaSpesa.length) {
     </li>
     `;
 }
+
+function generateListItem(item) {
+  return `
+    <li class="listItem">
+        <h1 class="nameObject">${item}</h1>
+        <div class="container_right_icon">
+            <button class="btnFunction" onclick="removeItem()">
+                <i class="fa-regular fa-trash-can"></i>
+            </button>
+        </div>
+    </li>
+  `;
+}
+
+function renderShoppingList() {
+  let li = '';
+  for (let i = 0; i < listaSpesa.length; i++) {
+    li += generateListItem(listaSpesa[i]);
+  }
+  shoppingList.innerHTML = li;
+}
+
+renderShoppingList();
+
+let input = document.querySelector("#inputList");
+let addButton = document.querySelector("#btnSub");
+
+addButton.addEventListener("click", function() {
+  let newItem = input.value;
+  if (newItem.trim() !== '') {
+    listaSpesa.push(newItem);
+    input.value = "";
+    renderShoppingList();
+  }
+});
 
 function removeItem(index) {
     listaSpesa.splice(index, 1);
