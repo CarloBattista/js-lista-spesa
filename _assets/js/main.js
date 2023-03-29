4e50cd3643f90c8d373474a678f2ddb900c1763c
CarloBattista <carlitobatti@gmail.com>
2023-03-29 14:57:05 +0200

    aggiunta lista spesa tramite js

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -5,15 +5,31 @@ basta che si vedano) gli elementi della lista individualmente con un ciclo while
 
 */
 
+// Lista della spesa
 let listaSpesa = [
     "Pane",
     "Patate",
     "Olio",
     "Pasta",
     "Burro",
-    "Pomodori",
-    "Insalata",
-    "Zucchero",
-    "Riso",
-    "olive",
 ];
+
+let spesa = 0
+let shoppingList = document.querySelector(".shoppingList");
+let li = '';
+
+while (spesa < listaSpesa.length) {
+    spesa++
+    li += `
+    <li class="listItem">
+        <h1 class="nameObject">Pane</h1>
+        <div class="container_right_icon">
+            <button class="btnFunction" onclick="removeItem()">
+                <i class="fa-regular fa-trash-can"></i>
+            </button>
+        </div>
+    </li>
+    `;
+}
+
+shoppingList.innerHTML = li;
